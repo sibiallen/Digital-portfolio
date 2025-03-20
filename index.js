a3aa63627f97cddd65ae44800235558e07b4dda8
sibiallen <sibiallen2005@gmail.com>
2025-03-20 23:47:25 +0530

    Sibi Allen

---
+++ index.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const sections = document.querySelectorAll("section");
+
+    const options = {
+        threshold: 0.2
+    };
+
+    const observer = new IntersectionObserver(function(entries, observer) {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("visible");
+            }
+        });
+    }, options);
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+});
